c1b44022dd09c51f7ffbfc29ef3b8429698e7160
Isaac Navajas <isaacnavajas@gmail.com>
2020-10-16 14:54:21 +0200

    otry

--- src/Routers.js
+++ src/Routers.js
@@ -39,7 +39,7 @@ function Router() {
             <Route exact path="/wordpress" component={Wordpress}/>
             
             
-            <Route component={Error}/>
+            <Route path="/a" component={Error}/>
   
           </Switch>
         </Layout>
